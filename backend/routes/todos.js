0ca9e13c8a69a385ed41ece228b037ef6cab9104
bhutuklearning <amritanshugoutam.dev@gmail.com>
2025-07-28 14:42:27 +0530

    Important Changes

--- backend/routes/todos.js
+++ backend/routes/todos.js
@@ -20,7 +20,16 @@ router.post('/', auth, createTodo);
 // Get all todos for the authenticated user
 router.get('/', auth, getTodos);
 
-// Get all todos for the authenticated user
+// Admin route to get all todos
+router.get('/admin', auth, roleCheck(['admin']), adminGetAllTodos);
+
+// Admin route to get all users and their todos
+router.get('/admin/users', auth, roleCheck(['admin']), adminGetUsersWithTodos);
+
+// Admin route to delete a todo by ID
+router.delete('/admin/:id', auth, roleCheck(['admin']), adminDeleteTodo);
+
+// Get a specific todo by ID
 router.get('/:id', auth, getTodosById);
 
 // Update a specific todo by ID
@@ -28,14 +37,5 @@ router.put('/:id', auth, updateTodo);
 
 // Delete a specific todo by ID
 router.delete('/:id', auth, deleteTodo);
-
-// Admin route to get all todos
-router.get('/admin', auth, roleCheck(['admin']), adminGetAllTodos);
-
-// Admin route to delete a todo by ID
-router.delete('/admin/:id', auth, roleCheck(['admin']), adminDeleteTodo);
-
-// Admin route to get all users and their todos
-router.get('/admin/users', auth, roleCheck(['admin']), adminGetUsersWithTodos);
 
 export default router;
