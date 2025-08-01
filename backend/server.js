4ecc2e7bb0a9da33e086383b7be939e4be9b90c5
bhutuklearning <amritanshugoutam.dev@gmail.com>
2025-08-01 10:45:10 +0530

    Keeping the server Alive

--- backend/server.js
+++ backend/server.js
@@ -46,6 +46,11 @@ app.use(errorHandler);
 
 // }
 
+app.get('/health', (req, res) => {
+    res.send('OK');
+});
+
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
