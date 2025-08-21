1bd2d484a81443fbb2437c54eb27693ea2a68de0
bhutuklearning <amritanshugoutam.dev@gmail.com>
2025-08-21 09:23:19 +0530

    API Updated

--- frontend/js/config.js
+++ frontend/js/config.js
@@ -1,7 +1,7 @@
 // js/config.js
 
 // Replace this with your deployed backend API URL
-const API_BASE_URL = "https://todo-backend-zbqu.onrender.com";
+const API_BASE_URL = "https://todo-app-trial.appwrite.network/";
 
 // Export for use in other JS files
 export default API_BASE_URL;
